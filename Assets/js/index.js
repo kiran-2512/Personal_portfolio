deccdfec0cc9b1084b37d80db251122b36a9a84c
kiran <kirangowda2512@gmail.com>
2024-10-10 00:13:49 +0530

    first commit

---
+++ Assets/js/index.js
@@ -0,0 +1,7 @@
+/*  Navigation Bar Click Function*/
+function myFunction(x) {
+  x.classList.toggle("change");
+  const navBar = document.querySelector('.navigation-bar');
+  navBar.classList.toggle('active');
+}
+
