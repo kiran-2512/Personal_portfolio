b93474e5c7ceeb672de666eec682d70f6c5bcd2d
kiran <kirangowda2512@gmail.com>
2024-10-16 22:08:30 +0530

    fixed some errors

--- Assets/js/index.js
+++ Assets/js/index.js
@@ -1,12 +1,12 @@
-/*  Navigation Bar Click Function*/
+// Navigation Bar Click Function 
 function myFunction(x) {
   x.classList.toggle("change");
   const navBar = document.querySelector('.navigation-bar');
   navBar.classList.toggle('active');
 }
-
+//Scroll Functionality 
 function scrollToSection(event, sectionId) {
   event.preventDefault();
   document.getElementById(sectionId).scrollIntoView({
     behavior: 'smooth'
-  });
+})};
